73018a2180396fd57ba0f36da39d353d3e7dbdac
Robinson_Xavier <robinson.webzen@gmail.com>
2022-09-19 07:44:35 -0300

    added new getProduct function

--- src/routes/products.routers.js
+++ src/routes/products.routers.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { productsList } from "../controllers/products.controller.js";
+import { productsList, getProduct } from "../controllers/products.controller.js";
 
 const router = Router();
 
 router.get("/products", productsList);
+router.get("/products/:productId", getProduct);
 
 export default router;
